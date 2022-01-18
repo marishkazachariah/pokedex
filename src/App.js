b793238d455193c8c5758b01ced040d89bd5c7ac
Marishka Zachariah <marishka.zachariah@gmail.com>
2022-01-18 14:57:33 +0100

    connecting all routes using react-router-dom and api with ApolloProvider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
-import PokemonsContainer from './containers/PokemonsContainer';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import PokemonContainer from "./containers/PokemonContainer";
+import { Routes, Route } from "react-router-dom";
+import Favourites from "./containers/FavouritesContainer";
+import Navbar from "./components/Navbar";
 
 function App() {
   const client = new ApolloClient({
-    uri: 'https://dex-server.herokuapp.com/'
+    uri: "https://dex-server.herokuapp.com/",
   });
 
   return (
     <ApolloProvider client={client}>
-      <main>
-        <PokemonsContainer />
-      </main>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<PokemonContainer />} />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
     </ApolloProvider>
   );
 }
