5085b187217670e15646750eb647ad853c533287
Marishka Zachariah <marishka.zachariah@gmail.com>
2022-01-19 12:12:53 +0100

    reformat search bar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -3,9 +3,8 @@ import { Divider, Input } from "antd";
 export default function Searchbar(props) {
   return (
     <>
-      <Divider>Search Pokémon</Divider>
       <div type="flex" justify="center" align="middle">
-      <label >Search by name</label>
+      <label>Search by name</label>
       <Input type="text" onChange={props.handleSearch} placeholder="Bulbasaur" style={{ width: 200 }}/>
       </div>
     </>
